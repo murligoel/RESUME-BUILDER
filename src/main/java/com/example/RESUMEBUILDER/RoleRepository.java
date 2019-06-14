8a0be76b730e250c377bd3a83b3e65d71825150a
murligoel <murli.goel1999@gmail.com>
2019-06-14 17:54:43 +0530

    Added role Jpa repository which contain built in properties like findOne,findAll,save

---
+++ src/main/java/com/example/RESUMEBUILDER/RoleRepository.java
@@ -0,0 +1,7 @@
+package com.example.RESUMEBUILDER;
+
+import com.example.RESUMEBUILDER.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role,Long> {
+}
