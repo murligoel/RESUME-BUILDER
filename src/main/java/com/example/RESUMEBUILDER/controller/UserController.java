0cc79eecae11abe70446b0b06a60596cded2d0b3
murligoel <murli.goel1999@gmail.com>
2019-06-15 16:54:49 +0530

    Added contoller and  specified get mapping to get the registration page

---
+++ src/main/java/com/example/RESUMEBUILDER/controller/UserController.java
@@ -0,0 +1,30 @@
+package com.example.RESUMEBUILDER.controller;
+
+import com.example.RESUMEBUILDER.UserValidator;
+import com.example.RESUMEBUILDER.model.User;
+import com.example.RESUMEBUILDER.service.SecurityService;
+import com.example.RESUMEBUILDER.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private SecurityService securityService;
+
+    @Autowired
+    private UserValidator userValidator;
+
+    @GetMapping("/registration")
+    public String registration(Model model){
+        model.addAttribute("userForm",new User());
+
+        return "registration";
+    }
+
+}
