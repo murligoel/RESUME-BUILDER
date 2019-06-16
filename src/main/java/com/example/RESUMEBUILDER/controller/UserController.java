a3b731bd2ab52f261808b6f377c970e1093356b5
murligoel <murli.goel1999@gmail.com>
2019-06-16 14:52:18 +0530

    Added post method for the registration in the controller

--- src/main/java/com/example/RESUMEBUILDER/controller/UserController.java
+++ src/main/java/com/example/RESUMEBUILDER/controller/UserController.java
@@ -7,7 +7,10 @@ import com.example.RESUMEBUILDER.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -27,6 +30,26 @@ public class UserController {
         modelAndView.setViewName("registration");
         modelAndView.addObject("userForm", new User());
         return modelAndView;
+    }
+
+    @PostMapping("/registration")
+    public String registration(@ModelAttribute("userForm") User userForm, BindingResult bindingResult) {
+        userValidator.validate(userForm, bindingResult);
+
+        if (bindingResult.hasErrors()) {
+            return "registration";
+        }
+
+        userService.save(userForm);
+
+        securityService.autoLogin(userForm.getUsername(), userForm.getPasswordConfirm());
+
+        return "redirect:/welcome";
+    }
+
+    @GetMapping({"/", "/welcome"})
+    public String welcome(Model model) {
+        return "welcome";
     }
 
 }
