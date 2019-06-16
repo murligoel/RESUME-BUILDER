ade4da364d8160e47a2b24ba512beeec43cbd5e9
murligoel <murli.goel1999@gmail.com>
2019-06-16 14:43:29 +0530

    Added template for the registration page

--- src/main/java/com/example/RESUMEBUILDER/controller/UserController.java
+++ src/main/java/com/example/RESUMEBUILDER/controller/UserController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class UserController {
@@ -21,10 +22,11 @@ public class UserController {
     private UserValidator userValidator;
 
     @GetMapping("/registration")
-    public String registration(Model model){
-        model.addAttribute("userForm",new User());
-
-        return "registration";
+    public ModelAndView registration(Model model){
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("registration");
+        modelAndView.addObject("userForm", new User());
+        return modelAndView;
     }
 
 }
