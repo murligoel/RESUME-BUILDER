762d0423c3d7eb1f91a3c6129f3cb763cb11f702
murligoel <murli.goel1999@gmail.com>
2019-06-17 23:22:32 +0530

    Added login template and made changes in the controller

--- src/main/java/com/example/RESUMEBUILDER/controller/UserController.java
+++ src/main/java/com/example/RESUMEBUILDER/controller/UserController.java
@@ -47,6 +47,17 @@ public class UserController {
         return "redirect:/welcome";
     }
 
+    @GetMapping("/login")
+    public String login(Model model, String error, String logout) {
+        if (error != null)
+            model.addAttribute("error", "Your username and password is invalid.");
+
+        if (logout != null)
+            model.addAttribute("message", "You have been logged out successfully.");
+
+        return "login";
+    }
+
     @GetMapping({"/", "/welcome"})
     public String welcome(Model model) {
         return "welcome";
