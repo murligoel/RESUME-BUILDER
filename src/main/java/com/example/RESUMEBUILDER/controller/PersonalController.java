18c9171e6e993c8298a28056a48e554cff6adb49
murligoel <murli.goel1999@gmail.com>
2019-06-21 16:20:18 +0530

    Added id recognition

--- src/main/java/com/example/RESUMEBUILDER/controller/PersonalController.java
+++ src/main/java/com/example/RESUMEBUILDER/controller/PersonalController.java
@@ -5,6 +5,7 @@ import com.example.RESUMEBUILDER.service.PersonalService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -17,17 +18,19 @@ public class PersonalController {
     @Autowired
     private PersonalService personalService;
 
+
     @RequestMapping(value="/personal" ,method= RequestMethod.GET)
-    public ModelAndView personalForm(String msg){
+    public ModelAndView personalForm(String msg , Long fetched_id){
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.setViewName("personalForm");
         modelAndView.addObject("personal",new Personal());
         modelAndView.addObject("msg", msg);
+        modelAndView.addObject("fetched_id",fetched_id);
         return modelAndView;
     }
 
     @RequestMapping(value = "/personal", method = RequestMethod.POST)
-    public ModelAndView createBookPost(@Valid Personal personal, BindingResult bindingResult) {
+    public ModelAndView createBookPost(@Valid Personal personal, BindingResult bindingResult ) {
         ModelAndView modelAndView = new ModelAndView();
         if(bindingResult.hasErrors()){
             modelAndView.setViewName("personalForm");
@@ -36,17 +39,16 @@ public class PersonalController {
         }
         else {
             personalService.createPersonal(personal);
-            modelAndView = personalForm("Personal details has been uploaded");
+            modelAndView = personalForm("Personal details has been uploaded",personal.getId());
         }
         return modelAndView;
     }
 
-
-    @RequestMapping(value = "/resume1",method = RequestMethod.GET)
-    public ModelAndView getPersonalDetails(Long id){
+    @RequestMapping(value = "/resume1/{id}",method = RequestMethod.GET)
+    public ModelAndView getPersonalDetails(@PathVariable Long id){
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.setViewName("resume_template1");
-        modelAndView.addObject("personalDetails",personalService.getPersonalDetails(id));
+        modelAndView.addObject("personalId",personalService.getPersonalDetails(id));
         return modelAndView;
     }
 }
