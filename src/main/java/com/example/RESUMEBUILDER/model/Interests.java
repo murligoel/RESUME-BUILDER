087c0336e62183c6692ced1bc357975609e78ba2
murligoel <murli.goel1999@gmail.com>
2019-07-01 10:46:40 +0530

    Created interest model

---
+++ src/main/java/com/example/RESUMEBUILDER/model/Interests.java
@@ -0,0 +1,22 @@
+package com.example.RESUMEBUILDER.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "interest")
+public class Interests {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column
+    @ElementCollection(targetClass=String.class)
+    private List<String> interests;
+}
